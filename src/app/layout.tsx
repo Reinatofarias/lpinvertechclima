a3854bb1f1ca8a49fb23f2b7aa16e91f73fc7732
Reinatofarias <reinatofarias@users.noreply.github.com>
2026-06-02 15:26:50 -0300

    chore: add Google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,29 +103,22 @@ export default function RootLayout({
           }}
         />
 
-        {/* GTM placeholder - replace GTM-XXXXXXX with real ID */}
-        {/* 
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-XXXXXXX');`
-        }} />
-        */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18203758951"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18203758951');
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col">
-        {/* GTM noscript placeholder */}
-        {/*
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        */}
+
         {children}
       </body>
     </html>
